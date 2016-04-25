61a3ff33040c239a33447dc59c674580ee886535
Frazier Mork <fraziermork@gmail.com>
2016-04-25 02:34:45 -0700

    got add button to work

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,53 @@
 import React from 'react';
-import Note from './Note.jsx';
+import uuid from 'node-uuid';
+// import Note from './Note.jsx';
 
 export default class App extends React.Component {
-  render() {
-    return <Note />;
+  constructor(props){
+    super(props);
+    this.state = {
+      notes: [
+        { 
+          id: uuid.v4(),
+          task: 'task 1'
+        }, 
+        {
+          id: uuid.v4(),
+          task: 'task 2'
+        },
+        {
+          id: uuid.v4(),
+          task: 'task 3'
+        }, 
+        {
+          id: uuid.v4(),
+          task: 'task 4'
+        }
+      ]
+    };
   }
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  };
+  
+  render() {
+    let notes = this.state.notes;
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <ul>
+          {notes.map((note) => {
+            return <li key={note.id}>{note.task}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+  
+  
 }
